df4eed791e4e16c7beb1e0236ebe6ec31a2e532a
Ravenx0x0 <110428658+Ravenx0x0@users.noreply.github.com>
2025-12-23 17:38:33 +0300

    Add names to user authentication URL patterns

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignupView, LoginView, MeView
 
 urlpatterns = [
-    path("signup/", SignupView.as_view()),
-    path("login/", LoginView.as_view()),
-    path("me/", MeView.as_view()),
+    path("signup/", SignupView.as_view(), name="auth-signup"),
+    path("login/", LoginView.as_view(), name="auth-login"),
+    path("me/", MeView.as_view(), name="auth-me"),
 ]
